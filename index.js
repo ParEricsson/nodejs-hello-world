9319d7ec4200b4fffbb88512af132666afc4a69a
Pär Ericsson <par.ericsson@cybercom.com>
2021-04-22 08:48:11 +0200

    Added sister

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
 
-    response.end("Hello World! my mother and father and brother");
+    response.end("Hello World! my mother and father and brother and sister");
 });
 
 var port = 80;
